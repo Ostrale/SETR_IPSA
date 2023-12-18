684a731fce146d56c04ff026925a0b95c7ab403b
Enzo Scafi <108572670+Ostrale@users.noreply.github.com>
2023-12-18 11:51:26 +0100

    RR start

---
+++ TP0/RR.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd>
+
+struct RR
+{
+    int Ci; // computation time
+    int Ti; // period
+    char name[10];
+};
+
+int main(int argc, char *argv[]){
+
+}
